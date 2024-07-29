1ada1eb0169207bad630a7b6c43bbfadc9bc6472
harryrowlands <rowlands.h02@gmail.com>
2024-07-29 02:14:48 +0000

    Just has conversation with Ashley about getting this up and stored on a new branch. An incomplete implementation of ReportBuilder.

---
+++ custom/report_builder/src/Plugin/rest/resource/CreateReportBuilderResource.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\report_builder\Plugin\rest\resource;
+
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+use Drupal\Core\KeyValueStore\KeyValueStoreInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\rest\ModifiedResourceResponse;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Drupal\report_builder\Entity\ReportBuilder;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Route;
+
+/**
+ * Represents create_report_builder records as resources.
+ *
+ * @RestResource (
+ *   id = "create_report_builder_resource",
+ *   label = @Translation("create_report_builder"),
+ *   uri_paths = {
+ *     "canonical" = "/rest/report/create",
+ *     "create" = "/rest/report/create"
+ *   }
+ * )
+ *
+ * @DCG
+ * The plugin exposes key-value records as REST resources. In order to enable it
+ * import the resource configuration into active configuration storage. An
+ * example of such configuration can be located in the following file:
+ * core/modules/rest/config/optional/rest.resource.entity.node.yml.
+ * Alternatively, you can enable it through admin interface provider by REST UI
+ * module.
+ * @see https://www.drupal.org/project/restui
+ *
+ * @DCG
+ * Notice that this plugin does not provide any validation for the data.
+ * Consider creating custom normalizer to validate and normalize the incoming
+ * data. It can be enabled in the plugin definition as follows.
+ * @code
+ *   serialization_class = "Drupal\foo\MyDataStructure",
+ * @endcode
+ *
+ * @DCG
+ * For entities, it is recommended to use REST resource plugin provided by
+ * Drupal core.
+ * @see \Drupal\rest\Plugin\rest\resource\EntityResource
+ */
+final class CreateReportBuilderResource extends ResourceBase {
+
+  /**
+   * The key-value storage.
+   */
+  private readonly KeyValueStoreInterface $storage;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    KeyValueFactoryInterface $keyValueFactory,
+    AccountProxyInterface $currentUser,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->storage = $keyValueFactory->get('create_report_builder_resource');
+    $this->currentUser = $currentUser;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new self(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('rest'),
+      $container->get('keyvalue'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * Returns next available ID.
+   */
+  private function getNextId(): int {
+    $ids = \array_keys($this->storage->getAll());
+    return count($ids) > 0 ? max($ids) + 1 : 1;
+  }
+  
+  /**
+   * Responds to POST requests and saves the new record.
+   */
+  public function post(array $data): ModifiedResourceResponse {
+    $data['id'] = $this->getNextId();
+    $this->storage->set($data['id'], $data);
+    $this->logger->notice('Created new create_report_builder record @id.', ['@id' => $data['id']]);
+    // Return the newly created record in the response body.
+    return new ModifiedResourceResponse($data, 201);
+  }
+
+}
